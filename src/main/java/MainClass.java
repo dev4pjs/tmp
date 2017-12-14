8383b9b798686af07f057b059697e208a4622d72
Purujit Saha <dev4pjs@gmail.com>
2017-12-14 00:41:47 -0500

    New Final Update!

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,5 @@
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Welcome to GIT Basics,");
+        System.out.println("Welcome to GIT Basics, ");
     }
 }
